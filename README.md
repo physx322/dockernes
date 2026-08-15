f9679e9f58375de83c13d3d3ecebbf0ad54d1728
PhysX <angledroit05@proton.me>
2026-08-15 17:37:47 +0200

    feat: update README and service configuration; enhance main logic for container management

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Dockernes is a small docker container orchestrator (like [docker swarm](https://docs.docker.com/engine/swarm/)) writen in Rust
 
 # Roadmap
-- [ ] 
+- [] Implements replicas of container
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,95 @@
-use std::{env, fs};
-use bollard::{Docker, query_parameters::{CreateImageOptionsBuilder}};
+use bollard::{
+    Docker,
+    plugin::ContainerCreateBody,
+    query_parameters::{
+        CreateContainerOptionsBuilder, CreateImageOptionsBuilder, SearchImagesOptionsBuilder,
+    },
+};
 use futures_util::StreamExt;
+use std::{collections::HashMap, env, fs};
 
 mod config;
-fn main() {
+
+#[tokio::main]
+async fn main() {
     let mut args: pico_args::Arguments = pico_args::Arguments::from_env();
-    let command: String = args.free_from_str().unwrap();
+
+    let command: String = match args.free_from_str() {
+        Ok(cmd) => cmd,
+        Err(_) => {
+            println!("Usage: program <command>");
+            return;
+        }
+    };
 
     match command.as_str() {
-        "run" => { 
-            start_container();
+        "run" => {
+            if let Err(e) = start_container().await {
+                eprintln!("Error running container: {}", e);
+            }
         }
-        _ => println!("Uknown command")
+        _ => println!("Unknown command"),
     }
 }
 
 async fn start_container() -> Result<(), Box<dyn std::error::Error>> {
     println!("starting creating the Container");
-    // detect current directory et search for service_dckrnes.toml file
+
     let cwd: std::path::PathBuf = env::current_dir().unwrap();
-    let service_file: std::path::PathBuf = cwd.join("service_dckrnes.toml");
+    let service_file: std::path::PathBuf = cwd.join("./service-dckrnes.toml");
     let docker = Docker::connect_with_local_defaults()?;
 
-
-    // if service_file exist, try parsing them, and extract value to put on a variables.
     if service_file.exists() {
-        println!("Service file found, try parsing");
+        println!("Service file found, try parsing...");
+
         let config_str: String = fs::read_to_string(service_file).expect("Failed to read file");
         let config: config::Dockernes = toml::from_str(&config_str).expect("Failed to parse TOML");
+
         let ctnr_name: String = config.service.name;
         let image_name: String = config.service.image;
-        let replicas: u32 = config.service.replica;
-        
+        let _replicas: u32 = config.service.replica;
+
         if docker.inspect_container(&ctnr_name, None).await.is_ok() {
             println!("Container already exist")
         } else {
-            let option = CreateImageOptionsBuilder::default().from_image(&image_name).build();
-            docker.create_image(Some(option), None, None)
-            .collect::<Vec<_>>()
-            .await;
+            let option = CreateImageOptionsBuilder::default()
+                .from_image(&image_name)
+                .build();
+
+            let mut filters = HashMap::new();
+            filters.insert("until", vec!["10m"]);
+
+            let search_options = SearchImagesOptionsBuilder::default()
+                .term(&image_name)
+                .filters(&filters)
+                .build();
+
+            if docker.search_images(search_options).await.is_ok() {
+                println!("Image found !");
+
+                let image_created = docker
+                    .create_image(Some(option), None, None)
+                    .collect::<Vec<_>>()
+                    .await;
+
+                println!("Image pulled successfully")
+            } else {
+                println!("Image not found, check your service file.")
+            }
+
+            let options = CreateContainerOptionsBuilder::default()
+                .name(&ctnr_name)
+                .build();
+
+            let config = ContainerCreateBody {
+                image: Some(()),
+                ..Default::default()
+            };
+
+            docker.start_container(&ctnr_name, options);
         }
+    } else {
+        println!("Service file not found at {:?}", service_file)
     }
     Ok(())
 }
-
